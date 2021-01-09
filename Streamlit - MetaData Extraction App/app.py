a823df6ce51d03a5b9345e5b9b2339b8d09d2d05
rainbowmoonlight <46021863+rainbowmoonlight@users.noreply.github.com>
2021-01-08 23:09:15 -0800

    update metadata extraction app

--- Streamlit - MetaData Extraction App/app.py
+++ Streamlit - MetaData Extraction App/app.py
@@ -14,6 +14,9 @@ matplotlib.use('Agg')
 # For Images
 from PIL import Image
 import exifread
+import os
+from datetime import datetime
+
 # For Audio
 import mutagen
 
@@ -31,6 +34,15 @@ HTML_BANNER = """
     </div>
     """
 
+# Functions
+@st.cache
+def load_image(image_file):
+	img = Image.open(image_file)
+	return img
+
+# Function to Get Human Readable Time
+def get_readable_time(mytime):
+	return datetime.fromtimestamp(mytime).strftime('%Y-%m-%d-%H:%M')
 
 # App Structure
 def main():
@@ -44,10 +56,79 @@ def main():
 
 	if choice == 'Home':
 		st.subheader("Home")
+		# Image
+		st.image(load_image("images/metadataextraction_app.png"))
+		# Desription
+
 		st.write(metadata_wiki)
+
+		# Expanders & Columns
+		col1, col2, col3 = st.beta_columns(3)
+		with col1:
+			with st.beta_expander('Get Image Metadata 📷'):
+				st.info("Image Metadata")
+				st.markdown("📷")
+				st.text("Upload JPEG,JPG,PNG Images")
+
+		with col2:
+			with st.beta_expander('Get Audio Metadata 🔉'):
+				st.info("Audio Metadata")
+				st.markdown("🔉")
+				st.text("Upload Mp3,Ogg")
+
+		with col3:
+			with st.beta_expander('Get Document Metadata 📄📁'):
+				st.info("Document Files Metadata")
+				st.markdown("📄📁")
+				st.text("Upload PDF,Docx")
+
+
 
 	elif choice == "Image":
 		st.subheader("Image MetaData Extraction")
+		image_file = st.file_uploader("Upload Image", type =["png","jpg","jpeg"])
+		if image_file is not None:
+			# Binary Byte
+			# st.write(type(image_file))
+			# st.write(dir(image_file))
+			with st.beta_expander("File Stats"):
+				file_details = {"FileName":image_file.name,
+								"FileSize":image_file.size,
+								"FileType":image_file.type}
+				st.write(file_details)
+
+				statinfo = os.stat(image_file.readable())
+				st.write(statinfo)
+				stats_details = {"Accessed_Time":get_readable_time(statinfo.st_atime),
+				"Creation_Time":get_readable_time(statinfo.st_ctime),
+				"Modified_Time":get_readable_time(statinfo.st_mtime)
+				}
+				st.write(stats_details)
+
+				# Combine All Details
+				file_details_combined = {"FileName":image_file.name,
+								"FileSize":image_file.size,
+								"FileType":image_file.type,
+								"Accessed_Time":get_readable_time(statinfo.st_atime),
+								"Creation_Time":get_readable_time(statinfo.st_ctime),
+								"Modified_Time":get_readable_time(statinfo.st_mtime)}
+
+				# Convert to DataFrame
+				df_file_details = pd.DataFrame(list(file_details_combined.items()), columns=["Meta Tags","Value"])
+				st.dataframe(df_file_details)
+
+			# Layouts
+			c1,c2 = st.beta_columns(2)
+			with c1:
+				with st.beta_expander("View Image"):
+					img= load_image(image_file)
+					st.image(img, width=250, height=250)
+
+			with c2:
+				with st.beta_expander("Default(JPEG)"):
+					st.info("Using PILLOW")
+					
+
 
 	elif choice == "Audio":
 		st.subheader("Audio MetaData Extraction")
@@ -57,6 +138,9 @@ def main():
 
 	elif choice == "About":
 		st.subheader("MetaData Extraction App")
+		# Image
+		st.image(load_image("images/metadataextraction_app.png"))
+		st.text("Built with Streamlit")
 		
 
 
