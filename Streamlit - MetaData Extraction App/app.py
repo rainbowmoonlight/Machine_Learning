8c96e8db39ae901a6483b014a20a822fc75a1024
rainbowmoonlight <46021863+rainbowmoonlight@users.noreply.github.com>
2021-01-09 01:34:31 -0800

    update MetaData Extraction App

--- Streamlit - MetaData Extraction App/app.py
+++ Streamlit - MetaData Extraction App/app.py
@@ -16,6 +16,7 @@ from PIL import Image
 import exifread
 import os
 from datetime import datetime
+import base64
 
 # For Audio
 import mutagen
@@ -43,6 +44,63 @@ def load_image(image_file):
 # Function to Get Human Readable Time
 def get_readable_time(mytime):
 	return datetime.fromtimestamp(mytime).strftime('%Y-%m-%d-%H:%M')
+
+# Get Image GeoTags
+from PIL.ExifTags import TAGS, GPSTAGS
+def get_exif(filename):
+	exif = Image.open(filename)._getexif()
+
+	if exif is not None:
+		for key, value in exif.items():
+			name = TAGS.get(key, key)
+			exif[name] = exif.pop(key)
+
+		if 'GPSInfo' in exif:
+			for key in exif['GPSInfo'].keys():
+				name = GPSTAGS.get(key, key)
+				exif['GPSInfo'][name] = exif['GPSInfo'].pop(key)
+
+	return exif
+
+# def get_coordinates(info):
+#     for key in ['Latitude', 'Longitude']:
+#         if 'GPS'+key in info and 'GPS'+key+'Ref' in info:
+#             e = info['GPS'+key]
+#             ref = info['GPS'+key+'Ref']
+#             info[key] = ( str(e[0][0]/e[0][1]) + '°' +
+#                           str(e[1][0]/e[1][1]) + '′' +
+#                           str(e[2][0]/e[2][1]) + '″ ' +
+#                           ref )
+
+#     if 'Latitude' in info and 'Longitude' in info:
+#         return [info['Latitude'], info['Longitude']]
+
+# def get_decimal_coordinates(info):
+#     for key in ['Latitude', 'Longitude']:
+#         if 'GPS'+key in info and 'GPS'+key+'Ref' in info:
+#             e = info['GPS'+key]
+#             ref = info['GPS'+key+'Ref']
+#             info[key] = ( e[0][0]/e[0][1] +
+#                           e[1][0]/e[1][1] / 60 +
+#                           e[2][0]/e[2][1] / 3600
+#                         ) * (-1 if ref in ['S','W'] else 1)
+
+#     if 'Latitude' in info and 'Longitude' in info:
+#         return [info['Latitude'], info['Longitude']]
+
+
+import time
+timestr = time.strftime("%Y%m%d-%H%M%S")
+
+ # Fxn to Download
+def make_downloadable(data):
+	csvfile = data.to_csv(index=False)
+	b64 = base64.b64encode(csvfile.encode()).decode() # B64 encoding
+	st.markdown("### ** Download CSV File ** ")
+	new_filename = "metadata_result_{}.csv".format(timestr)
+	href = f'<a href="data:file/csv;base64,{b64}" download="{new_filename}">Click Here!</a>'
+	st.markdown(href,unsafe_allow_html=True)
+
 
 # App Structure
 def main():
@@ -88,7 +146,7 @@ def main():
 		st.subheader("Image MetaData Extraction")
 		image_file = st.file_uploader("Upload Image", type =["png","jpg","jpeg"])
 		if image_file is not None:
-			# Binary Byte
+			# UploadFile Class is File-Like Binary Byte
 			# st.write(type(image_file))
 			# st.write(dir(image_file))
 			with st.beta_expander("File Stats"):
@@ -127,7 +185,49 @@ def main():
 			with c2:
 				with st.beta_expander("Default(JPEG)"):
 					st.info("Using PILLOW")
+					img = load_image(image_file)
+					st.write(dir(img))
+					img_details = {"format":img.format,
+									"format_desc":img.format_description,
+									"filename":img.filename,
+									"size":img.size,
+									"height":img.height,
+									"width":img.width,
+									"info":img.info
+									}
+					#st.write(img_details)
+					df_img_details_default = pd.DataFrame(list(img_details.items()),
+					columns=["Meta Tags","Value"])
+					st.dataframe(df_img_details_default)
+
+			# Layouts For Forensic
+			fcol1,fcol2 = st.beta_columns(2)
+
+			with fcol1:
+				with st.beta_expander("Exifread Tool"):
+					meta_tags = exifread.process_file(image_file)
+					# st.write(meta_tags)
+
+					df_img_details_exifread = pd.DataFrame(list(meta_tags.items()),
+					columns=["Meta Tags","Value"])
+					st.dataframe(df_img_details_exifread)
+
+			# with fcol2:
+			# 	with st.beta_expander("Image Geo-Coordinates"):
+			# 		img_details_with_exif = get_exif(image_file)
+			# 		try:
+			# 			gpg_info = img_details_with_exif
+			# 		except:
+			# 			gpg_info = "None Found"
+
+			# 		st.write(gpg_info)
+			# 		img_coordinates = get_decimal_coordinates(gpg_info)
+			# 		st.write(img_coordinates)	
 					
+			with st.beta_expander("Download Results"):
+				final_df = pd.concat([df_file_details, df_img_details_default, df_img_details_exifread])
+				st.dataframe(final_df)
+				make_downloadable(final_df)
 
 
 	elif choice == "Audio":
